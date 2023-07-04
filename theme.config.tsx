cc7918b60c3b83bc93b418b5f069b8e8b09ab499
Ubuntu <ubuntu@mail.catgirlsaresexy.org>
2023-07-04 03:26:34 +0000

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Refractor</span>,
+  project: {
+    link: 'https://github.com/thebozzz34/Refractor',
+  },
+  chat: {
+    link: 'https://dsc.gg/t0ast',
+  },
+  docsRepositoryBase: 'https://github.com/thebozzz34/refractordocs',
+  footer: {
+    text: 'My GitHub',
+  },
+}
+
+export default config
